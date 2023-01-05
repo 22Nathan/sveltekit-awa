8e35055bfbbe103d5924f407e642b114cdec44e7
22Nathan <64403796+22Nathan@users.noreply.github.com>
2023-01-05 19:59:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sveltekit-awa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/sveltejs-kit-template-default-jmdngb)
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,50 @@
+<script>
+	import "../app.css";
+</script>
+
+<div class="app">
+	
+<header class="">
+	<nav class="">
+
+		<a href="/" class="" data-sveltekit-preload-data="">
+		</a>
+
+		<!-- mobile nav btn -->
+		<button class="" aria-label="Toggle navigation"></button> 
+		
+		<!-- mobile nav menu -->
+		<div class="sm:hidden">
+			<a href="/" class="nav-link-mobile" data-sveltekit-preload-data=""></a> 
+			<a href="/" class="nav-link-mobile" data-sveltekit-preload-data=""></a> 
+			<a href="/" class="nav-link-mobile" data-sveltekit-preload-data=""></a> 
+			<a href="/" class="nav-link-mobile" data-sveltekit-preload-data=""></a> 
+			<a href="/" class="nav-link-mobile" data-sveltekit-preload-data=""></a>
+		</div> 
+		
+		
+		<div class="nav-desktop-menu max-sm:hidden ">
+			<a href="/changelog" class="nav-link " data-sveltekit-preload-data="">Updates</a> 
+			<a href="/blog" class="nav-link " data-sveltekit-preload-data="">Blog</a> 
+			<a href="/design" class="nav-link " data-sveltekit-preload-data="">Design</a> 
+			<a href="https://docs.raster.app" class="nav-link max-lg:hidden" rel="noopener noreferrer" target="_blank" data-sveltekit-reload="">Docs
+				<svg viewBox="0 0 24 24" class="w-4 ml-1 opacity-40" fill="none" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+					<path d="M7 17L17 7"></path>
+					<path d="M7 7h10v10"></path>
+				</svg>
+			</a> 
+			<a href="/pricing" class="nav-link " data-sveltekit-preload-data="">Pricing</a> 
+			<a href="/sign-in" class="button px-4 ml-2">
+				<svg viewBox="0 0 24 24" class="mr-2 w-4" fill="none" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+					<path d="M15 3h4a2 2 0 012 2v14a2 2 0 01-2 2h-4"></path>
+					<path d="M10 17l5-5-5-5"></path>
+					<path d="M15 12H3"></path>
+				</svg>
+				Sign in
+			</a>
+		</div>
+	</nav>
+</header>
+
+
+</div>
